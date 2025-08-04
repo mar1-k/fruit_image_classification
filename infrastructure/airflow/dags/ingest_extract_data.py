83df0a3a44d6e55f6c12ee93f7acfd2cb55585dc
mar1 <14811869+mar1-k@users.noreply.github.com>
2025-08-04 03:23:08 -0400

    Test dag

---
+++ infrastructure/airflow/dags/ingest_extract_data.py
@@ -0,0 +1,55 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+import os
+
+# Define the default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define the DAG
+with DAG(
+    'ingest_to_datalake',
+    default_args=default_args,
+    description='A DAG to upload CSV files from a local directory to GCS',
+    schedule_interval='@once',  # Run once as soon as possible
+    start_date=datetime.now(),
+    is_paused_upon_creation=False,
+    tags=['data_engineering', 'ingestion', 'data_lake', 'extract'],
+) as dag:
+    # Define the BashOperator to execute curl command to download the file
+    download_task = BashOperator(
+        task_id='execute_download_task',
+        bash_command='curl -o /opt/airflow/data/2024_linkedin_scraped_data.zip https://storage.googleapis.com/project-raw-data/2024_linkedin_scraped_data.zip'
+    )
+
+    # Define the BashOperator to execute unzip command to unzip the file
+    unzip_task = BashOperator(
+        task_id='execute_unzip_task',
+        bash_command='unzip -o /opt/airflow/data/2024_linkedin_scraped_data.zip -d /opt/airflow/data/'
+    )
+
+    # Define the LocalFilesystemToGCSOperator to upload the CSVs to the datalake
+    upload_task = LocalFilesystemToGCSOperator(
+        task_id='upload_job_to_gcs',
+        bucket=os.environ.get("BUCKET_NAME"),
+        src="/opt/airflow/data/*.csv",  # Upload all CSV files in the directory
+        dst="data/"  # Destination directory in GCS
+    )
+
+    # Define the TriggerDagRunOperator to trigger the transform DAG
+    trigger_transform_dag_task = TriggerDagRunOperator(
+        task_id = 'trigger_transform_dag_task',
+        trigger_dag_id = 'transform_data_with_spark'
+    )
+
+    # Set the task dependencies
+    download_task >> unzip_task >> upload_task >> trigger_transform_dag_task
